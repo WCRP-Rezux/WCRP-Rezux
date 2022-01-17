fd7bcb8d4c78cfe58eca944aae93ed24eda65057
WCRP-Rezux <97872383+WCRP-Rezux@users.noreply.github.com>
2022-01-17 00:37:50 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ RegisterCommand("aop", function(source, args, rawCommand)
     if IsPlayerAceAllowed(source, "AOP-StaffCommands") then
         if #args >= 0 then
             WCurAop = table.concat(args, " ")
-            TriggerClientEvent("")
+            TriggerClientEvent("") 
         end
     end
 end)
