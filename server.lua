174c596c57531fc994d11f3266929e339a077a20
Sraist <ConnorTheDev01@gmail.com>
2022-01-17 01:33:31 -0600

    Hud-Changes 1.1

--- server.lua
+++ server.lua
@@ -1,4 +1,8 @@
-RegisterServerEvent('EventNameHere')
-AddEventHandler('EventNameHere', function(data)
-    
+RegisterCommand("aop", function(source, args, rawCommand)
+    if IsPlayerAceAllowed(source, "AOP-StaffCommands") then
+        if #args >= 0 then
+            WCurAop = table.concat(args, " ")
+            TriggerClientEvent("")
+        end
+    end
 end)
